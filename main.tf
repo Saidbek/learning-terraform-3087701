e9093f50bc48b603ac28ba095c3baccdbfa5caf2
Said Kaldybaev <said.kaldybaev@gmail.com>
2023-01-13 14:43:53 -0800

    fix security group typos

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  vpn_security_group_ids = [aws_security_group.blog.id]
+  vpc_security_group_ids = [aws_security_group.blog.id]
 
   tags = {
     Name = "HelloWorld"
@@ -37,7 +37,7 @@ resource "aws_security_group" "blog" {
 }
 
 resource "aws_security_group_rule" "blog_http_in" {
-  type        = "ingres"
+  type        = "ingress"
   from_port   = 80
   to_port     = 80
   protocol    = "tcp"
@@ -47,7 +47,7 @@ resource "aws_security_group_rule" "blog_http_in" {
 }
 
 resource "aws_security_group_rule" "blog_https_in" {
-  type        = "ingres"
+  type        = "ingress"
   from_port   = 443
   to_port     = 443
   protocol    = "tcp"
@@ -57,7 +57,7 @@ resource "aws_security_group_rule" "blog_https_in" {
 }
 
 resource "aws_security_group_rule" "blog_everything_out" {
-  type        = "engres"
+  type        = "egress"
   from_port   = 0
   to_port     = 0
   protocol    = "-1"
